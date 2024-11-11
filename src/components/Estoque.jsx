e562cd23b8912c0f2ca628c9af2ce763e2db599d
KanivetUchiha <erickazambuja.ms@gmail.com>
2024-11-11 17:48:57 -0300

    11/11

--- src/components/Estoque.jsx
+++ src/components/Estoque.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState } from "react"
+import { useEffect } from "react"
+import stilo from "../style/estoque.module.css"
 
-export default function App() {
-  
-  const [lista, setLista] = useState([]);
 
-   useEffect(() => { 
+export default function Estoque (){
+
+    const [lista, setLista] = useState([]);
+
+  useEffect(() => { 
     const receberListaProdutos = async () => {
         const resposta = await fetch('https://fakestoreapi.com/products');
         const dados = await resposta.json();
@@ -16,14 +19,16 @@ export default function App() {
   return (
     <>
       <h1>Lista de Produtos</h1>
-      <ul>
+      <ul className={stilo.ul}>
         {lista.map(produto => (
+          <div className={stilo.container}>
           <li key={produto.id}>
             <h2>{produto.title}</h2>
             <p>{produto.description}</p>
             <p>Preço: ${produto.price}</p>
             <img src={produto.image} alt={produto.title} width={100} />
           </li>
+          </div>
         ))}
       </ul>
     </>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Estoque from '../components/Estoque.jsx'
+import {useState} from 'react'
+
+export default function Home (){
+
+    return (
+<div><Estoque/></div>
+
+    )
+}
